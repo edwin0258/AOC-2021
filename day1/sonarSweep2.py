c5b1141b3baeece01481b760f4e2171f7e17a97b
Devin Miller <millerd4@sou.edu>
2021-12-01 17:56:12 -0800

    Refactored day one part two

--- day1/sonarSweep2.py
+++ day1/sonarSweep2.py
@@ -1,16 +1,11 @@
 f = open('sonarSweep.txt', 'r')
 
-[count, prev, windowSize, i, winSum] = [0, 0, 3, 0, 0]
+[count, prev, windowSize] = [0, 0, 3]
 
-dataArr = []
-for line in f:
-    dataArr.append(int(line))
+dataArr = [int(line) for line in f]
 
-windowArr = []
 for i, x in enumerate(dataArr):
     curr = sum(dataArr[i:i+windowSize])
-    windowArr.append(curr)
-
     if prev < curr:
         count += 1 
     prev = curr 
