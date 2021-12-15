f2de98d7d801aa2a4e5efb9523b9765c81bb94f5
Devin Miller <edwin0258@gmail.com>
2021-12-14 16:44:03 -0800

    Completed day 7

---
+++ day7/day7-2.js
@@ -0,0 +1,30 @@
+const fs = require('fs');
+
+fs.readFile('data.txt', (err, data) => {
+    if (err) throw err
+
+    let positions = {}; // store positions in groups
+    let maxPos = 0;
+    let minFuel = Infinity;
+    
+    data.toString().split('\n')[0].split(',').map(x => +x).forEach(pos => {
+        maxPos = (pos > maxPos) ? pos : maxPos; // used for for loop later
+        (pos in positions) ? positions[pos] += 1 : positions[pos] = 1;
+    })
+
+    // test how much fuel all crabs need to reach each i pos 
+    for(let i = 0; i <= maxPos; i++) {
+        let fuelNeeded = 0;
+        Object.keys(positions).forEach(pos => {
+            let distBetween = (pos > i) ? (pos - i) : (i - pos);
+            // calculate nth triangle number (part 2)
+            // https://www.mathsisfun.com/algebra/triangular-numbers.html
+            let trangularNum = distBetween * ((distBetween+1)/2);
+            fuelNeeded += (trangularNum * positions[pos]); // dist * all crabs at pos
+        })
+        // Does pos offer min fuel required or not
+        if(minFuel > fuelNeeded) minFuel = fuelNeeded;
+    }
+
+    console.log(minFuel);
+})
